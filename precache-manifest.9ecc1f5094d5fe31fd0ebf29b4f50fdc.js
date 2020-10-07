7434916b80fae4b0ed94dd3ce24130b352ed8ce1
Nikita-12345 <singh.nkta@gmail.com>
2020-10-07 21:58:04 +0530

    Updates

---
+++ precache-manifest.9ecc1f5094d5fe31fd0ebf29b4f50fdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e44111ee52edde2597ec8995c01c0813",
+    "url": "/my-task/index.html"
+  },
+  {
+    "revision": "b1e401af20f4948c6b87",
+    "url": "/my-task/static/css/2.2d63d0b4.chunk.css"
+  },
+  {
+    "revision": "a9481553ba93ea8c24ba",
+    "url": "/my-task/static/css/main.e2bdff01.chunk.css"
+  },
+  {
+    "revision": "b1e401af20f4948c6b87",
+    "url": "/my-task/static/js/2.534e965b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-task/static/js/2.534e965b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9481553ba93ea8c24ba",
+    "url": "/my-task/static/js/main.a5b64557.chunk.js"
+  },
+  {
+    "revision": "4522bbef6dcba90228d9",
+    "url": "/my-task/static/js/runtime-main.35ecdd31.js"
+  }
+]);
